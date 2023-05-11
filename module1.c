c995579d6d10b5f0a3e195560a6c19226e313a65
Boris V Gubarev <sargon811@yandex.ru>
2023-05-11 10:58:54 +0300

    Add the module_init/exit macros

--- module1.c
+++ module1.c
@@ -1,17 +1,21 @@
 
 #include <linux/module.h>
 #include <linux/printk.h>
+#include <linux/init.h>
 
-int init_module(void)
+static int __init mdl_init(void)
 {
-    pr_info("Hello!\n");
+	pr_info("Hello!\n");
 	return 0;
 }
 
-void cleanup_module(void)
+static void __exit mdl_exit(void)
 {
-    pr_info("Goodbye!\n");
+	pr_info("Goodbye!\n");
 }
+
+module_init(mdl_init);
+module_exit(mdl_exit);
 
 MODULE_LICENSE("GPL");
 
