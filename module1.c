5659616d48ba7916ee80b5c8ba5a0ac0d682a1b7
Boris V Gubarev <sargon811@yandex.ru>
2023-05-11 14:33:51 +0300

    Add command line arguments

--- module1.c
+++ module1.c
@@ -2,16 +2,30 @@
 #include <linux/module.h>
 #include <linux/printk.h>
 #include <linux/init.h>
+#include <linux/kernel.h>
+#include <linux/moduleparam.h>
+#include <linux/stat.h>
 
 MODULE_LICENSE("GPL");
 MODULE_DESCRIPTION("My Kernel Module");
 MODULE_AUTHOR("GBV");
 
-static int mdl_data __initdata = 255;
+static int my_int = 999;
+static char *my_string = "My First Kernel Module";
+
+module_param(my_int, int, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+MODULE_PARM_DESC(my_int, "An integer");
+
+module_param(my_string, charp, 0000);
+MODULE_PARM_DESC(my_string, "A character string");
+
+static int mdl_data __initdata = 555;
 
 static int __init mdl_init(void)
 {
 	pr_info("Loading module %d\n", mdl_data);
+	pr_info("Integer is %d\n", my_int);
+	pr_info("String is %s\n", my_string);
 	return 0;
 }
 
